c1ff6edd62a54e8ee111b35b53779fd1f7327891
dandrejszki <daniel.andrejszki@gmail.com>
2021-01-10 13:10:51 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,25 @@
+// const http = require('http');
+// const server = http.createServer((req, res) => {
+//   console.log(req.url, req.method, req.headers);
+//   //process.exit();
+//   res.setHeader("Content-Type", "text/html");
+//   res.write("<html>");
+//   res.write("<head><title>My First Page</title><head>");
+//   res.write("<body><h1>Hello world!</h1></body>");
+//   res.write("</html>");
+//   res.end();
+// });
+// server.listen(8080);
+
+
+// const express = require('express');
+
+// const app = express();
+
+// app.get('/', (req, res) => {
+//   res.send('Hi there');
+// });
+
+// app.listen(8080, () => {
+//   console.log('Listening on port 8080');
+// });
